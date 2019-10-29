6565e58a39586f87b9abb1d1f93921b2958c3e84
Sean Hellum <seanhellum45@gmail.com>
2019-10-29 17:34:12 +0000

    start project

--- app.ts
+++ app.ts
@@ -5,7 +5,4 @@ app.use(express.static("static"))
 app.get('/', function(req, res) {
     res.render("index.html")
 })
-app.get(/a/, function(req, res) {
-    res.send('the url has an a in it')
-})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
