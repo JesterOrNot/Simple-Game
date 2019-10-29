bc843bc37fa056c4137c80ff8dda9f8b11d52155
Sean Hellum <seanhellum45@gmail.com>
2019-10-29 17:13:27 +0000

    regex

--- app.ts
+++ app.ts
@@ -5,5 +5,7 @@ const port = 3000
 app.get('/', function (req, res) {
   res.send('Hello World From ExpressJS')
 })
-
+app.get(/a/, function (req, res) {
+  res.send('the url has an a in it')
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
